26aa4daf0bb3a1acdef77e6de948cf2adb429ac5
Raymond H. Tu <hh2@umd.edu>
2021-10-13 13:20:04 -0400

    reformat

--- src/trainer.py
+++ src/trainer.py
@@ -4,30 +4,32 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 # Create a simple dataset
+
+
 class MyDataset(object):
-  def __init__(self):
-    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    def __init__(self):
+        (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
-    # Preprocess the data (these are NumPy arrays)
-    x_train = x_train.reshape(60000, 784).astype("float32") / 255
-    x_test = x_test.reshape(10000, 784).astype("float32") / 255
+        # Preprocess the data (these are NumPy arrays)
+        x_train = x_train.reshape(60000, 784).astype("float32") / 255
+        x_test = x_test.reshape(10000, 784).astype("float32") / 255
 
-    y_train = y_train.astype("float32")
-    y_test = y_test.astype("float32")
+        y_train = y_train.astype("float32")
+        y_test = y_test.astype("float32")
 
-    # Reserve 10,000 samples for validation
-    self.x_val = x_train[-10000:]
-    self.y_val = y_train[-10000:]
-    self.x_train = x_train[:-10000]
-    self.y_train = y_train[:-10000]
+        # Reserve 10,000 samples for validation
+        self.x_val = x_train[-10000:]
+        self.y_val = y_train[-10000:]
+        self.x_train = x_train[:-10000]
+        self.y_train = y_train[:-10000]
 
 
 # Create a simple model for the dataset
 def my_model():
-  inputs = keras.Input(shape=(784,), name="digits")
-  x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
-  x = layers.Dense(64, activation="relu", name="dense_2")(x)
-  outputs = layers.Dense(10, activation="softmax", name="predictions")(x)
-  return keras.Model(inputs=inputs, outputs=outputs)
+    inputs = keras.Input(shape=(784,), name="digits")
+    x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
+    x = layers.Dense(64, activation="relu", name="dense_2")(x)
+    outputs = layers.Dense(10, activation="softmax", name="predictions")(x)
+    return keras.Model(inputs=inputs, outputs=outputs)
 
 # complete the training script below
--- test/test_trainer.py
+++ test/test_trainer.py
@@ -1,9 +1,11 @@
 import pytest
 from src.trainer import MyDataset, my_model
 
+
 @pytest.fixture
 def my_dataset():
     # return your dataset object
+
 
 @pytest.fixture
 def my_model():
